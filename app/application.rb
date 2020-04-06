bf619d0cc5d5902e22b5295419831d4a24f31b15
Jacqueline Freeland <secretartist272@gmail.com>
2020-04-06 14:52:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,8 +16,14 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |cart|
-        resp.write
+      if @@cart.empty?
+        @@cart.each do |item|
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
     else
       resp.write "Path Not Found"
     end
